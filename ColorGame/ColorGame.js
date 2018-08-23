2a608ee4c7db5103da2245a3cab3b6ec988f015f
Dong Yuan Yang <dyan263@UoA.auckland.ac.nz>
2018-08-23 18:09:37 +1200

    color game refactor - halfway there

--- ColorGame/ColorGame.js
+++ ColorGame/ColorGame.js
@@ -5,7 +5,7 @@ var colors = [
   "rgb(255, 0, 0)",
   "rgb(255, 0, 0)",
   "rgb(255, 0, 0)"
-]
+];
 
 var squares = document.querySelectorAll(".square");
 var goal;
@@ -15,19 +15,16 @@ for (var i = 0; i < squares.length; i++) {
   var r = Math.floor(Math.random() * 256);
   var g = Math.floor(Math.random() * 256);
   var b = Math.floor(Math.random() * 256);
-  var randC = "rgb(" + r + ", " + g + ", " + b + ")"
-  squares[i].style.backgroundColor = randC
-  if (i === goalnum){
+  var randC = "rgb(" + r + ", " + g + ", " + b + ")";
+  squares[i].style.backgroundColor = randC;
+  if (i === goalnum) {
     goal = randC;
   }
-  squares[i].addEventListener("click", function(){
-    if (this.style.backgroundColor === goal){
-      document.getElementById("correct").textContent = "Correct!"
-    }
-    else (
-      document.getElementById("correct").textContent = "Try Again!"
-    )
-  })
+  squares[i].addEventListener("click", function() {
+    if (this.style.backgroundColor === goal) {
+      document.getElementById("correct").textContent = "Correct!";
+    } else document.getElementById("correct").textContent = "Try Again!";
+  });
 }
 
 var colorDisplay = document.getElementById("colorDisplay");
