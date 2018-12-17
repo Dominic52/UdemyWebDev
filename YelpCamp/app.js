11ff46027ebd0329421d62bf1241bc6310f484c0
Dominic52 <dongyuanyang@hotmail.com>
2018-12-17 18:20:15 +1300

    add new campground functionality implemented

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -1,19 +1,46 @@
 var express = require("express");
 var app = express();
+var bodyparser = require("body-parser");
+
+
+
 
 app.set("view engine", "ejs");
+//Sets view engine to ejs. Allows references to ejs files to omit .ejs suffix
+app.use(bodyparser.urlencoded({extended: true}));
+
+
 
 app.get("/", function (req, res) {
+    //Get routing for landing page
     res.render("landing");
 })
 
+var campgrounds = [
+    //Temporary dummy campgrounds data
+    { name: "Salmon Creek", image: "https://farm4.staticflickr.com/3474/3723452014_69038c2ffa.jpg" },
+    { name: "Owen G Glen Building", image: "https://farm1.staticflickr.com/119/274197774_42179734d0.jpg" },
+    { name: "Joy Park", image: "https://farm5.staticflickr.com/4124/4972095496_e8c6be009d.jpg" }
+]
+
 app.get("/campgrounds", function (req, res) {
-    var campgrounds = [
-        { name: "Salmon Creek", image: "https://farm4.staticflickr.com/3474/3723452014_69038c2ffa.jpg" },
-        { name: "Owen G Glen Building", image: "https://farm1.staticflickr.com/119/274197774_42179734d0.jpg" },
-        { name: "Joy Park", image: "https://farm5.staticflickr.com/4124/4972095496_e8c6be009d.jpg" }
-    ]
+    //Get routing for campgrounds page
+    
     res.render("campgrounds", { campgrounds: campgrounds })
+})
+
+app.post("/campgrounds", function(req, res) {
+    //Post routing for new campgrounds
+    var name = req.body.name;
+    var image = req.body.image;
+    var newCampground = {name: name, image: image};
+    campgrounds.push(newCampground);
+    res.redirect("/campgrounds");
+})
+
+app.get("/campgrounds/new", function(req, res){
+    //New campground form page
+    res.render("new");
 })
 
 app.listen(3000, function () {
