ef1c5020c610cc5e488cb2ddb9cfa4b21daddad9
Dominic52 <dongyuanyang@hotmail.com>
2018-12-18 20:23:46 +1300

    added bootstrap styling

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -20,6 +20,15 @@ var campgrounds = [
     //Temporary dummy campgrounds data
     { name: "Salmon Creek", image: "https://farm4.staticflickr.com/3474/3723452014_69038c2ffa.jpg" },
     { name: "Owen G Glen Building", image: "https://farm1.staticflickr.com/119/274197774_42179734d0.jpg" },
+    { name: "Joy Park", image: "https://farm5.staticflickr.com/4124/4972095496_e8c6be009d.jpg" },
+    { name: "Salmon Creek", image: "https://farm4.staticflickr.com/3474/3723452014_69038c2ffa.jpg" },
+    { name: "Owen G Glen Building", image: "https://farm1.staticflickr.com/119/274197774_42179734d0.jpg" },
+    { name: "Joy Park", image: "https://farm5.staticflickr.com/4124/4972095496_e8c6be009d.jpg" },
+    { name: "Salmon Creek", image: "https://farm4.staticflickr.com/3474/3723452014_69038c2ffa.jpg" },
+    { name: "Owen G Glen Building", image: "https://farm1.staticflickr.com/119/274197774_42179734d0.jpg" },
+    { name: "Joy Park", image: "https://farm5.staticflickr.com/4124/4972095496_e8c6be009d.jpg" },
+    { name: "Salmon Creek", image: "https://farm4.staticflickr.com/3474/3723452014_69038c2ffa.jpg" },
+    { name: "Owen G Glen Building", image: "https://farm1.staticflickr.com/119/274197774_42179734d0.jpg" },
     { name: "Joy Park", image: "https://farm5.staticflickr.com/4124/4972095496_e8c6be009d.jpg" }
 ]
 
