edfe21c58b854d60d59ffedadd1591b86d87743c
Dominic52 <dongyuanyang@hotmail.com>
2018-12-20 22:53:46 +1300

    linked mongoDB

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -1,57 +1,77 @@
 var express = require("express");
 var bodyparser = require("body-parser");
+var mongoose = require("mongoose");
 
 var app = express();
 
+//MongoDB connection
+mongoose.connect("mongodb://localhost:27017/yelp_camp", {useNewUrlParser: true});
 
 
-app.set("view engine", "ejs");
 //Sets view engine to ejs. Allows references to ejs files to omit .ejs suffix
+app.set("view engine", "ejs");
+
+//Sets bodyparser to return URLencoded format
 app.use(bodyparser.urlencoded({extended: true}));
 
 
+//Database Schema
+var campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String
+});
+
+var Campground = mongoose.model("Campground", campgroundSchema);
+
+//Test creates new campground object to be saved to database
+// Campground.create(
+//     { name: "Owen G Glen Building", image: "https://farm1.staticflickr.com/119/274197774_42179734d0.jpg" }, function(err, campground){
+//         if (err){
+//             console.log("SOMETHING WENT WRONG");
+//         } else {
+//             console.log("CAMPGROUND ADDED TO DB");
+//         }
+//     }
+// );
 
 app.get("/", function (req, res) {
     //Get routing for landing page
     res.render("landing");
-})
-
-var campgrounds = [
-    //Temporary dummy campgrounds data
-    { name: "Salmon Creek", image: "https://farm4.staticflickr.com/3474/3723452014_69038c2ffa.jpg" },
-    { name: "Owen G Glen Building", image: "https://farm1.staticflickr.com/119/274197774_42179734d0.jpg" },
-    { name: "Joy Park", image: "https://farm5.staticflickr.com/4124/4972095496_e8c6be009d.jpg" },
-    { name: "Salmon Creek", image: "https://farm4.staticflickr.com/3474/3723452014_69038c2ffa.jpg" },
-    { name: "Owen G Glen Building", image: "https://farm1.staticflickr.com/119/274197774_42179734d0.jpg" },
-    { name: "Joy Park", image: "https://farm5.staticflickr.com/4124/4972095496_e8c6be009d.jpg" },
-    { name: "Salmon Creek", image: "https://farm4.staticflickr.com/3474/3723452014_69038c2ffa.jpg" },
-    { name: "Owen G Glen Building", image: "https://farm1.staticflickr.com/119/274197774_42179734d0.jpg" },
-    { name: "Joy Park", image: "https://farm5.staticflickr.com/4124/4972095496_e8c6be009d.jpg" },
-    { name: "Salmon Creek", image: "https://farm4.staticflickr.com/3474/3723452014_69038c2ffa.jpg" },
-    { name: "Owen G Glen Building", image: "https://farm1.staticflickr.com/119/274197774_42179734d0.jpg" },
-    { name: "Joy Park", image: "https://farm5.staticflickr.com/4124/4972095496_e8c6be009d.jpg" }
-]
+});
 
 app.get("/campgrounds", function (req, res) {
     //Get routing for campgrounds page
-    
-    res.render("campgrounds", { campgrounds: campgrounds })
-})
+    //Gets all campgrounds from database
+    Campground.find({}, function(err, allcampgrounds){
+        if (err){
+            console.log("SOMETHING WENT WRONG");
+        } else {
+            console.log("Here is a list of all campgrounds");
+            res.render("campgrounds", {campgrounds: allcampgrounds});
+        }
+    });
+});
 
 app.post("/campgrounds", function(req, res) {
     //Post routing for new campgrounds
     var name = req.body.name;
     var image = req.body.image;
     var newCampground = {name: name, image: image};
-    campgrounds.push(newCampground);
-    res.redirect("/campgrounds");
-})
+    //Creates new campground and saves to database
+    Campground.create(newCampground, function(err, newlyCreated){
+        if (err){
+            console.log(err);
+        } else {
+            res.redirect("/campgrounds");
+        }
+    });
+});
 
 app.get("/campgrounds/new", function(req, res){
     //New campground form page
     res.render("new");
-})
+});
 
 app.listen(3000, function () {
     console.log("Yelp Camp server has started");
-})
+});
---
+++ YelpCamp/dbstart.bat
@@ -0,0 +1,1 @@
+start call "C:\Program Files\MongoDB\Server\4.0\bin\mongod.exe" --dbpath "/Users/User/Documents/UdemyWebDev/YelpCamp/data"
